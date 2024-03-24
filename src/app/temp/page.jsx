6505afcd509c14123a70cdbd028ffa6e1dd4d06b
Christina Lin <chrstina.lin@gmail.com>
2024-03-24 14:12:08 -0400

    calendar colours

--- src/app/temp/page.jsx
+++ src/app/temp/page.jsx
@@ -12,20 +12,53 @@ import {
 } from '@schedule-x/calendar'
 
 import '@schedule-x/theme-default/dist/index.css'
+import '@/calendar.css'
+
+import { createDragAndDropPlugin } from '@schedule-x/drag-and-drop'
+import { createEventModalPlugin } from '@schedule-x/event-modal'
+import { createResizePlugin } from '@schedule-x/resize'
 
 export default function Temp() {
 
     const calendar = useCalendarApp({
-        defaultView: viewMonthGrid.name,
-        views: [viewDay, viewWeek, viewMonthGrid, viewMonthAgenda],
+        defaultView: viewWeek.name,
+        views: [viewDay, viewWeek],
+        
         events: [
             {
-                id: '1',
-                title: 'Event 1',
-                start: '2023-12-16',
-                end: '2023-12-16',
+                id: 2,
+                title: 'Breakfast with Sam',
+                description: 'Discuss the new project',
+                location: 'Starbucks',
+                start: '2023-11-29 05:00',
+                end: '2023-11-29 06:00',
+                calendarId: 'low'
+            },
+            {
+                id: 3,
+                title: 'Gym',
+                start: '2023-11-27 06:00',
+                end: '2023-11-27 07:00',
+                calendarId: 'high'
+            },
+            {
+                id: 5,
+                title: 'Some appointment',
+                people: ['John'],
+                start: '2024-03-23 03:00',
+                end: '2024-03-23 04:30',
+                calendarId: 'high'
+            },
+            {
+                id: 6,
+                title: 'Other appointment',
+                people: ['Susan', 'Mike'],
+                start: '2024-03-23 03:00',
+                end: '2024-03-23 04:00',
+                calendarId: 'medium',
             },
         ],
+        plugins: [createDragAndDropPlugin(), createEventModalPlugin(), createResizePlugin()]
     })
 
     return (
